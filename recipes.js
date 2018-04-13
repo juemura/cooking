e58af935fd13a7456181a3a11c1379bbef79b728
Michelle <michellewong693@gmail.com>
2018-04-12 18:00:39 -0700

    ingredients page

--- recipes.js
+++ recipes.js
@@ -1,12 +1,22 @@
 var recipes = function(game){}
  
 recipes.prototype = {
+    preload: function(){ 
+       
+        this.game.load.image("fried_rice","images/friedrice.jpg");      
+	},
   	create: function(){
+      
 //        add recipes 
 //        create a play button
-		var friedChickenButton = this.game.add.button(250,100,"fried_chicken",this.ingredientsAndInstructions,this);
-        friedChickenButton.scale.setTo(.5,.5);
+		var friedChickenButton = this.game.add.button(250,100,"fried_rice",this.ingredientsAndInstructions,this);
+        friedChickenButton.scale.setTo(.1,.1);
 		friedChickenButton.anchor.setTo(0.5,0.5);
+        this.game.add.text(
+          0, 0,  // x, y position
+          "Fried Rice", 
+          { fontSize: "32px", fill: "#fff" }
+        );
 	},
 	ingredientsAndInstructions: function(){
 		this.game.state.start("ingredients");
